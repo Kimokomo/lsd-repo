5d18d856838d759d3433cb3bd2d87a563f71bf21
Karim Yassin <fhb201276@fh-vie.ac.at>
2024-07-28 19:22:41 +0200

    adapted OdsFileReader

--- lsd-importer/src/main/java/App.java
+++ lsd-importer/src/main/java/App.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
 
-        OdsFileReader odsFileReader = new OdsFileReader(new File("C:\\Users\\karim\\Downloads\\G_UNT2_Statistisches_Unternehmen_Hauptergebnisse_nach_Beschaeftigtengroessenklassen_2022.ods"));
+        OdsFileReader odsFileReader = new OdsFileReader(new File("D:\\lsd_\\lsd-importer\\src\\main\\resources\\G_UNT2_Statistisches_Unternehmen_Hauptergebnisse_nach_Beschaeftigtengroessenklassen_2022.ods"));
         List<LsdExchangeEntity> readAllList = odsFileReader.readAll();
         readAllList.forEach(System.out::println);
     }
